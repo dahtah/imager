d2cf360f4f3b4b59bc96b438ae887ea53278ac5a
Simon Barthelme <simon.barthelme@gipsa-lab.fr>
2015-07-17 14:45:40 +0200

    Added safeguard against multiple inclusions of Rcpp.h

--- inst/include/imager.h
+++ inst/include/imager.h
@@ -1,6 +1,13 @@
 #ifndef IMAGER_H
 #define IMAGER_H
+
+
 #include "CImg.h"
+
+#if defined(Rcpp_hpp) && !defined(COMPILING_IMAGER)
+#error "The file 'Rcpp.h' should not be included. Please correct to include only 'imager.h'."
+#endif
+
 #include <Rcpp.h>
 #include "wrappers.h"
 typedef cimg_library::CImg<double> CId;
