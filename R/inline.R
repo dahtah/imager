a8d9174e02b2ccd9dc89c27ab86a9f000afc003c
Simon Barthelme <simon.barthelme@gipsa-lab.fr>
2016-06-03 13:27:47 +0200

    fixed inline.R, added test

--- R/inline.R
+++ R/inline.R
@@ -1,7 +1,41 @@
+#Sets up a Rcpp inline plugin 
+#Much of this is adapted from RcppGSL by Romain Francois and Dirk Eddelbuettel
+ 
+.pkgglobalenv <- new.env(parent=emptyenv())
 
-inlineCxxPlugin <- Rcpp:::Rcpp.plugin.maker(
-	include.before = "#include <imager.h>;",
+.onLoad <- function(libname, pkgname) {
+    if (.Platform$OS.type=="windows") {
+        imager_cflags <- "-Dcimg_r_mode -fpermissive"
+        imager_libs   <- "-lgdi32"
+    } else {
+        imager_cflags <- "-Dcimg_r_mode -fpermissive"
+        imager_libs   <- "-lX11"
+    }
+    assign("imager_cflags", imager_cflags, envir=.pkgglobalenv)
+    assign("imager_libs", imager_libs, envir=.pkgglobalenv)
+}
+
+LdFlags <- function(print = TRUE) {
+    if (print) cat(.pkgglobalenv$imager_libs) else .pkgglobalenv$imager_libs
+}
+
+CFlags <- function(print = TRUE) {
+    if (print) cat(.pkgglobalenv$imager_cflags) else .pkgglobalenv$imager_cflags
+}
+
+inlineCxxPlugin <- function(...) {
+    plugin <- Rcpp::Rcpp.plugin.maker(
+        include.before = "#include <imager.h>;",
         include.after = "using namespace cimg_library;",
-	libs           = "-lX11", 
-	package        = "imager"
-)
+	package        = "imager",
+        Makevars = NULL, Makevars.win = NULL
+    )
+    settings <- plugin()
+    settings$env$PKG_LIBS <- LdFlags(FALSE)
+    settings$env$PKG_CPPFLAGS <- CFlags(FALSE)
+    ## settings$configure <- readLines( system.file( "skeleton", "configure", package = "imager" ) )
+    ## settings$configure.win <- readLines( system.file( "skeleton", "configure.win", package = "imager" ) )
+    ## settings$Makevars.in <- readLines( system.file( "skeleton", "Makevars.in", package = "imager" ) )
+    settings                            
+}
+
---
+++ tests/testthat/test_cpp_api.R
@@ -0,0 +1,16 @@
+library(imager)
+library(Rcpp)
+
+test_that("cpp_plugin",{
+foo.inline <- "
+NumericVector foo(NumericVector inp)
+{
+    CImg<double> img = as<CImg<double> >(inp);
+    img.blur(3).erode(2);
+    return wrap(img);
+} "
+
+cppFunction(foo.inline,depends="imager")
+im <- grayscale(boats)
+expect_equal(im %>% isoblur(3) %>% erode_square(2),foo(im))
+})
