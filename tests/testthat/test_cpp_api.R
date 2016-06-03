df8c4adc6a6f209518760fcb9d2c70bd11a94313
Simon Barthelme <simon.barthelme@gipsa-lab.fr>
2016-06-03 14:02:21 +0200

    Fixed test

--- tests/testthat/test_cpp_api.R
+++ tests/testthat/test_cpp_api.R
@@ -2,14 +2,15 @@ library(imager)
 library(Rcpp)
 
 test_that("cpp_plugin",{
-foo.inline <- "
+
+    foo.inline <- "
 NumericVector foo(NumericVector inp)
 {
     CImg<double> img = as<CImg<double> >(inp);
     img.blur(3).erode(2);
     return wrap(img);
 } "
-
+    Sys.setenv("R_TESTS" = "")
 cppFunction(foo.inline,depends="imager")
 im <- grayscale(boats)
 expect_equal(im %>% isoblur(3) %>% erode_square(2),foo(im))
