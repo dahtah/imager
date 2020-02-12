d3bb310f3c358eb07cc07db71c6a23776f0f1d3f
Shota Ochi <shotaochi1990@gmail.com>
2020-02-12 16:54:06 +0900

    use png instead of jpg

--- tests/testthat/test_load_save_images.R
+++ tests/testthat/test_load_save_images.R
@@ -4,7 +4,7 @@ library(Rcpp)
 test_that("load_and_save_images",{
     im <- imfill(10,10,val=c(0,0,0))
      
-    ff <- tempfile(fileext=".jpg")
+    ff <- tempfile(fileext=".png")
     save.image(im,ff)
     im2 <- load.image(ff)
     unlink(ff)
